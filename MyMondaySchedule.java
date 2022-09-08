26704250d08b70ba7276661feb88880598ba3253
ethanhopkins <112411800+ethanhopkins@users.noreply.github.com>
2022-09-08 12:12:59 -0400

    Add files via upload

---
+++ MyMondaySchedule.java
@@ -0,0 +1,19 @@
+public class MyMondaySchedule
+{
+
+public static void main(String[] args)
+{
+
+System.out.println("On Monday I wake up at " + 6 + "AM");
+System.out.println("After waking up I get ready by taking a shower and eating breakfast");
+System.out.println("At around " + 9 + ":" + 30 + " I leave the house in order to get to computer science on time");
+System.out.println("After computer science ends at " + 11 + ":" + 50 + ", I move on to my next class, Theology, for an hour and " + 15 + "minutes");
+System.out.println("At this point in the day, I usually have about " + 2 + " hours to spare, so I either work on any class work or try making pixel art");
+System.out.println("After the break, I have my last class of the day, History, at " + 3 + ", and after that I drive back home, usually arriving at " + 5 + ":" + 30);
+System.out.println("Once home, I eat supper");
+System.out.println("For an hour or two, I end up watching a show or playing a game");
+System.out.println("Around " + 9 + " or so, I take my medicine and stay up for another hour");
+System.out.println("Finally, I go to bed sometime between " + 10 + " and " + 12);
+
+}
+}
